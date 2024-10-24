a71eaac9e9787e7b2293036de7782130c30eac18
AuraMc-ext <auramcpll@gmail.com>
2024-10-24 17:39:14 +0200

    Update build.gradle.kts

--- ZaluknijProvider/build.gradle.kts
+++ ZaluknijProvider/build.gradle.kts
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    authors = listOf("Auramc")
 
     /**
      * Status int as the following:
