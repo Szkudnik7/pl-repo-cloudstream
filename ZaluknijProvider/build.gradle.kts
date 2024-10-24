f8c32117ff053bd55114f3a21ffc1c8dfa324505
AuraMc-ext <auramcpll@gmail.com>
2024-10-24 17:43:41 +0200

    Update build.gradle.kts

--- ZaluknijProvider/build.gradle.kts
+++ ZaluknijProvider/build.gradle.kts
@@ -3,7 +3,7 @@ version = 2
 
 
 cloudstream {
-    language = "pl"
+    language = "en"
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
@@ -18,8 +18,8 @@ cloudstream {
      * */
     status = 1
     tvTypes = listOf(
-        "Movie",
-        "TvSeries"
+        "Moviesfrserte",
+        "TvStsdtseteries"
     )
 
     iconUrl = "https://www.google.com/s2/favicons?domain=zaluknij.xyz&sz=%size%"
