0969f1743dff00c2be6250323876fa8bb0f45e90
AuraMc-ext <auramcpll@gmail.com>
2024-10-25 23:28:21 +0200

    Update ZaluknijProvider.kt

--- ZaluknijProvider/src/main/kotlin/com/lagradost/ZaluknijProvider.kt
+++ ZaluknijProvider/src/main/kotlin/com/lagradost/ZaluknijProvider.kt
@@ -19,26 +19,23 @@ open class EkinoProvider : MainAPI() {
         TvType.Movie
     )
 
-override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
+override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse { 
     val document = app.get(mainUrl).document
-    val listElements = document.select("mainWrap") // (zmien)
+    val lists = document.select("mainWrap") // (zmien)
     val categories = ArrayList<HomePageList>()
-    for (listElement in listElements) {
-        val title = capitalizeString(listElement.select("h3").text().lowercase().trim())
-        val items = listElement.select(".nowa-poster").map { i ->
-            val poster = i.attr("src")
-            MovieSearchResponse(
-                title,
-                poster,
-                request.name, // (zmien)
-                TvType.Movie,
-                null, // (zmien)
-                0 // (zmien)
-            )
+    for (l in lists) {
+        val title = capitalizeString(l.parent()!!.select("h3").text().lowercase().trim()) 
+        val items = l.select(".nowa-poster").map { i -> 
+            val a = i.parent()!!
+            val name = a.attr("title")
+            val href = a.attr("href") // (zmien)
+            val poster = i.select("img[src]").attr("src")
+            val year = a.select(".year").text().toIntOrNull() // (zmien)
+            MovieSearchResponse( name, href, this.name, TvType.Movie, poster, year ) 
         }
-        categories.add(HomePageList(title, items))
+        categories.add(HomePageList(title, items)) 
     }
-    return HomePageResponse(categories)
+    return HomePageResponse(categories) 
 }
 
     override suspend fun search(query: String): List<SearchResponse> {
