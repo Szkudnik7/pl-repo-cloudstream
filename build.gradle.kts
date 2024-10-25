bd060e81fb572326859742c68af6e2fc909bb31e
AuraMc-ext <auramcpll@gmail.com>
2024-10-25 18:08:17 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
 buildscript {
     repositories {
         google()
@@ -8,7 +14,6 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-        // Universal build config
         classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.15.1")
     }
 }
@@ -22,10 +27,15 @@ allprojects {
     }
 }
 
-plugins {
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+cloudstream {
+    language = "pl"
+    authors = listOf("AuraMc")
+    tvTypes = listOf(TvType.Movie, TvType.TvSeries)
+    iconUrl = "https://www.google.com/s2/favicons?domain=cda-hd.cc&sz=%size%"
 }
 
-//tasks.register<Delete>("clean") {
-//    delete(rootProject.layout.buildDirectory)
-//}
+dependencies {
+    implementation("com.lagradost.cloudstream3:cloudstream-core:1.0.0") // Upewnij się, że masz tę wersję
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0") // Jackson
+    implementation("org.jsoup:jsoup:1.14.3") // Jsoup
+}
