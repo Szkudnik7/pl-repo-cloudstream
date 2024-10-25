ee2afe90d76118e7954cd922d1836ad260672c86
AuraMc-ext <auramcpll@gmail.com>
2024-10-25 18:00:04 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,86 +1,83 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
-import com.android.build.gradle.BaseExtension
+package com.lagradost
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
-        maven("https://jitpack.io")
-    }
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-    }
-}
+open class EkinoProvider : MainAPI() {
+    override var mainUrl = "https://ekino-tv.pl/"
+    override var name = "Ekino"
+    override var lang = "pl"
+    override val hasMainPage = true
+    override val usesWebView = true
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get(mainUrl).document
+        val categories = ArrayList<HomePageList>()
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+        // Zbieramy wszystkie kategorie
+        document.select(".item-list").forEach { listElement ->
+            val title = listElement.parent().select("h3").text().trim()
+            val items = listElement.select(".poster").mapNotNull { posterElement ->
+                val linkElement = posterElement.parent()
+                val href = linkElement.attr("href")
+                val name = linkElement.attr("title")
+                val poster = posterElement.select("img[src]").attr("src")
+                val year = linkElement.select(".year").text().toIntOrNull()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
-    }
-
-    android {
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(33)
-            targetSdk = 33
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8" // Required
-                // Disables some unnecessary features
-                freeCompilerArgs = freeCompilerArgs +
-                        "-Xno-call-assertions" +
-                        "-Xno-param-assertions" +
-                        "-Xno-receiver-assertions"
+                // Tworzymy obiekt MovieSearchResponse
+                MovieSearchResponse(name, href, name, TvType.Movie, poster, year)
             }
+            categories.add(HomePageList(title, items))
+        }
+
+        return HomePageResponse(categories)
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val url = "$mainUrl/search/qf"
+        val document = app.post(url, mapOf("q" to query)).document
+        val movies = document.select("#mres > div") // Używamy selektora do wyników wyszukiwania
+
+        return movies.mapNotNull { movieElement ->
+            val href = movieElement.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val img = movieElement.selectFirst("a > img[src]")?.attr("src")
+            val name = movieElement.selectFirst(".title")?.text() ?: return@mapNotNull null
+
+            MovieSearchResponse(name, href, name, TvType.Movie, img, null)
         }
     }
 
-    dependencies {
-        val apk by configurations
-        val implementation by configurations
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+        val title = document.select("span[itemprop=name]").text()
+        val posterUrl = document.select("#single-poster > img").attr("src")
+        val plot = document.select(".description").text()
 
-        // Stubs for all Cloudstream classes
-        apk("com.lagradost:cloudstream3:pre-release")
+        return MovieLoadResponse(title, url, name, TvType.Movie, "", posterUrl, null, plot)
+    }
 
-        // these dependencies can include any of those which are added by the app,
-        // but you dont need to include any of them if you dont need them
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
-        implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.4.4") // http library
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-	   implementation("org.jsoup:jsoup:1.13.1") // html parser
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = if (data.startsWith("http")) app.get(data).document else Jsoup.parse(data)
+        document.select(".link-to-video").forEach { item ->
+            val decoded = base64Decode(item.select("a").attr("data-iframe"))
+            val link = tryParseJson<LinkElement>(decoded)?.src ?: return@forEach
+            loadExtractor(link, subtitleCallback, callback)
+        }
+        return true
     }
 }
 
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
-}
+data class LinkElement(
+    @JsonProperty("src") val src: String
+)
